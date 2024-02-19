605747d979f67065ca64ce151d8a852958eb4003
NoahHenriques <nbh21@case.edu>
2024-02-19 18:26:24 -0500

    Added a read, eval-program, and eval-statement
    Added a general file reader, syntax-tree reader, and started on a statement reader that would call the different statements' functions. However, I believe that I implemented the last statement incorrectly

--- SLICode.rkt
+++ SLICode.rkt
@@ -1,7 +1,41 @@
 #lang racket
 
 ; require parser
-require "simpleParser.rkt"
+(require "simpleParser.rkt")
+
+;reads the file and passes it to an evaluator
+(define read
+  (lambda filename
+    (eval-program (parser filename))))
+
+;parses the file into its syntax tree
+(define eval-program
+  (lambda syntax-tree
+    (cond
+      ((null? syntax-tree) '())
+      (else
+       (eval-statement (car syntax-tree))
+       (eval-program (cdr syntax-tree))))))
+
+;reads through each statement and determines how it should be treated
+;I think I implemented this wrong
+(define eval-statement
+  (lambda statement
+    (cond
+      ((null? statement) '())
+      ;I'm not sure how to
+      ((list? (car statement)) null) 
+      ;list of expressions that should call smaller functions
+      ((eq? (car statement) 'var) null)
+      ((eq? (car statement) '=) null)
+      ((eq? (car statement) 'return) null)
+      ((eq? (car statement) 'if) null)
+      ((eq? (car statement) 'var) null)
+      ((eq? (car statement) 'while) null)
+      ;may need to have more statements
+      (else
+       (eval-statement (cdr statement))))))
+
 
 ; interpret function
 
