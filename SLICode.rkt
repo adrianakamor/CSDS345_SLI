c782aa8b84fe2d829135f5426d15afe67c327a0d
adrianakamor <atk88@case.edu>
2024-02-19 21:53:27 -0500

    Added Math Exprs and Variable
    Outlined beginning of statements for math operators, and gave variable state assignment cons lines

--- SLICode.rkt
+++ SLICode.rkt
@@ -20,13 +20,16 @@
 ;reads through each statement and determines how it should be treated
 ;I think I implemented this wrong
 (define eval-statement
-  (lambda statement
+  (lambda (statement state)
     (cond
       ((null? statement) '())
       ;I'm not sure how to
       ((list? (car statement)) null) 
       ;list of expressions that should call smaller functions
-      ((eq? (car statement) 'var) null)
+      ((eq? (car statement) 'var)
+       (if (= (length statement) 2)
+           (cons (cons (car (cdr statement)) #f) state)
+           (cons (cons (car (cdr statement) (eval-expressions (car (cdr (car statement))) state))))
       ((eq? (car statement) '=) null)
       ((eq? (car statement) 'return) null)
       ((eq? (car statement) 'if) null)
@@ -34,9 +37,31 @@
       ((eq? (car statement) 'while) null)
       ;may need to have more statements
       (else
-       (eval-statement (cdr statement))))))
+       (eval-statement (cdr statement))))))))
 
-
+; other helper method for logic/math expressions
+  (define eval-expressions
+    (lambda (expression state)
+      (cond
+        ((eq? (car expression) '+))
+        ((eq? (car expression) '-))
+        ((eq? (car expression) '*))
+        ((eq? (car expression) '/))
+        ((eq? (car expression) '%))
+        ((eq? (car expression) '==))
+        ((eq? (car expression) '!=))
+        ((eq? (car expression) '<))
+        ((eq? (car expression) '>))
+        ((eq? (car expression) '<=))
+        ((eq? (car expression) '>=))
+        ((eq? (car expression) '&&))
+        ((eq? (car expression) '||))
+        ((eq? (car expression) '!))
+        ((number? expression) expression)
+        ((boolean? expression) expression)
+        ((symbol? expression) (lookup expression state)))))
+        
+    
 ; interpret function
 
 ; binding pairs
