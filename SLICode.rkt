79972f1a9704f725ccd5d51d77cb9badcd27f400
Stephen Hogeman <ssh115@case.edu>
2024-03-21 00:33:56 -0400

    Changed the layering such that layers are added, unsure how to alter state such that layers are traversed

--- SLICode.rkt
+++ SLICode.rkt
@@ -4,7 +4,7 @@
 
 
 ; ------------------------------------------------------------
-; CSDS 345 - Simple Language Interpreter
+; CSDS 345 - Flow Control Interpreter
 ; Group 3: Noah Henriques, Stephen Hogeman, Adriana Kamor
 ; ------------------------------------------------------------
 
@@ -40,27 +40,27 @@
 ; M_state Function
 ; eval-statement: reads through each statement and determines how it should be treated depending on the keywords
 (define eval-statement
-  (lambda (statement states k)
+  (lambda (statement states)
     (cond
-      ((null? statement) (k (cdr states)))
-      ((list? (car statement)) (k null))
-      ((eq? (car statement) 'begin) (begin-block (cdr statement) (layer states)))
-      ((eq? (car statement) 'var) (declare-var statement states k))
-      ((eq? (car statement) '=) (init-assign (cadr statement) (cddr statement) states k))
-      ((eq? (car statement) 'return) (init-assign 'return (cdr statement) states k))
+      ((null? statement) (cdr states))
+      ((list? (car statement)) null)
+      ((eq? (car statement) 'begin) (begin-block (cdr statement) (new-layer states)))
+      ((eq? (car statement) 'var) (declare-var statement states))
+      ((eq? (car statement) '=) (init-assign (cadr statement) (cddr statement) states))
+      ((eq? (car statement) 'return) (init-assign 'return (cdr statement) states))
       ((eq? (car statement) 'if)
         (eval_expressions (cadr statement) states (lambda (v)
             (if-statement v
-              (eval-statement (caddr statement) states k)
-              (eval-statement (cadddr statement) states k) k))))
-      ((eq? (car statement) 'while) (while (cadr statement) (caddr statement) states k))
+              (eval-statement (caddr statement) states)
+              (eval-statement (cadddr statement) states)))))
+      ((eq? (car statement) 'while) (while (cadr statement) (caddr statement) states))
       ; I put a section for helpers below this section, but am not sure if we want to use
-      ; helpers or not for these.
+      ; helpers or not for these. 
       ; ((eq? (car statement) 'break) (break-helper states k))
       ; ((eq? (car statement) 'continue) (continue-helper states k))
       ; ((eq? (car statement) 'throw) (throw-helper states k))
       (else
-       (eval-statement (cdr statement) states k)))))
+       (eval-statement (cdr statement) states)))))
 
 ; M_value Function
 ; eval_expressions: reads through each statement and determines which expressions are used and how those expressions should be treated
@@ -69,9 +69,9 @@
       (cond
         ((number? expression)        expression)
         ((boolean? expression)       expression)
-        ((eq? expression #t)  'true)
-        ((eq? expression 'true) #t)
-        ((eq? expression #f) 'false)
+        ((eq? expression #t)     #t)
+        ((eq? expression 'true)  #t)
+        ((eq? expression #f)     #f)
         ((eq? expression 'false) #f)
         ((symbol? expression)        (lookup expression state))
         ((eq? (operator expression) '+)   (+ (eval_expressions (leftoperand expression) state) (eval_expressions (rightoperand expression) state)))
@@ -111,8 +111,33 @@
 ;(define throw-helper
   ;(lambda (state k)))
 
-; layer: adds an empty layer of (() ()) to the front of the current state, for the current block of code
-(define layer (lambda (state) (cons '(() ()) state)))
+; new-layer: adds an empty layer of (() ()) to the front of the current state, for the current block of code
+(define new-layer (lambda (state) (cons '(() ()) state)))
+
+; The general logic for traversing through the layers is to:
+;  A. Check the current layer to see if the desired value is there
+;  B. If there is the desired value, return both the layer number and index number of value?
+;  C. If not, go through next layer, incrementing counter of layer.
+; lookup-state: 
+(define lookup-state
+  (lambda (var state layer index)
+    (if (eq? -1 (lookup-layer (var (caar state) 0)))
+        (cons (car state) (lookup-state var (cdr state) (+ 1 layer) index))
+        layer)))
+
+; lookup-layer:
+(define lookup-layer
+  (lambda (var layer-state layer)
+    (if (null? layer-state)
+        -1
+        0)))
+        
+
+; replacer-layer:
+
+; replacer-state:
+
+
 ; ------------------------------------------------------------
 
 
@@ -122,16 +147,10 @@
 ; update-states: function that updates the states given a variable and a value
 (define update-states
   (lambda (vari val states)
-    (replacer (find-index vari (list(car states))) val states)))
-
-; append: function that appends two lists together; will be used for state assignment
-(define append
-  (lambda (lis1 lis2)
-	(if (null? lis1)
-    	lis2
-    	(cons (car lis1) (append(cdr lis1) lis2)))))
+    (replacer (find-index vari (list (car states))) val states)))
 
 ;find-index: function + helper that takes the states and a given variable, and finds the index of that variable in the first part of states
+; Needs to be greatly modified or discarded for part 2, as this won't be helpful since we have multiple layers
 (define find-index
   (lambda (x lst)
   (loop (car lst) x 0)))
@@ -144,16 +163,16 @@
 ; replacer: function + helper that takes an index and iterates through the second part of the states until it lands on that index,
 ; replacing the value there with the one given
 (define replacer
-  (lambda (index value lst)
-  (list (car lst) (replacer-helper (cadr lst) index value))))
+  (lambda (layer index value lst)
+  (list (car lst) (replacer-helper (cadr lst) layer index value))))
 (define replacer-helper
-  (lambda (lst index value)
+  (lambda (state layer index value)
   (if (= index 0)
       (cond
-        ((eq? value #t) (cons 'true (cdr lst)))
-        ((eq? value #f) (cons 'false (cdr lst)))
-        (else (cons value (cdr lst))))
-      (cons (car lst) (replacer-helper (cdr lst) (- index 1) value)))))
+        ((eq? value #t) (cons 'true (cdr state)))
+        ((eq? value #f) (cons 'false (cdr state)))
+        (else (cons value (cdr state))))
+      (cons (car state) (replacer-helper (cdr state) (- index 1) value)))))
 
 ; lookup-helper: helper that takes an index and iterates through the second part of the states until it lands on that index,
 ; returning that value
@@ -165,9 +184,10 @@
 
 ; create-pair: inserts a variable and value into the state given the variable expression and the state
 (define create-pair
-  (lambda (y x value)
-  (cons (cons y (car x))
-        (cons (cons value (cadr x)) '()))))
+  (lambda (var state value)
+  (cons (cons var (caar state))
+        (cons (cons value (caadr state)) '()))))
+
 ; ------------------------------------------------------------
 
 
@@ -187,7 +207,8 @@
          (if (not (null? (cddr statement)))
              (init-assign (cadr statement) (cddr statement) (create-pair (cadr statement) states '()))
              (create-pair (cadr statement) states '()))))))
-            
+; By changing it such that a pair is only created in the first layer of the state, then this should prevent variables being declared outside of their
+; scope. 
         
 ; init-assign: accepts the variable in question, an expression, and the current list of states ((...)(...))
 ;checks to make sure that the variable has been declared; if not (idk what it'll do lol)
@@ -236,8 +257,9 @@
 
 ; Assignment 2 - Syntax Tree Statements
 ; ------------------------------------------------------------
+; begin-block: Handles the case of a code block anywhere in the program
 (define begin-block
   (lambda (block states)
-    (eval-statement block states)))
-; Currently just a start, need to change many things with adding state, as well as the lookup function to be able to go through
-; multiple blocks
+    (if (null? block)
+        (cdr states)
+        (begin-block (cdr block) (eval-statement (car block) states)))))
